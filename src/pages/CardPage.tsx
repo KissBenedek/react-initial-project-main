137702a662ea96604b94b304ab2947338131c0a5
c9kisben <c9kisben@vasvari.org>
2025-02-24 10:16:47 +0100

    Kész az első feladat

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -1,14 +1,33 @@
-import { Card, CardText, ListGroup } from 'react-bootstrap';
+import { Button, Card, CardText, ListGroup } from 'react-bootstrap';
 
 const CardPage = () => {
     return (
-        <Card style={{ width: '200px', height: '200px', marginLeft: 'auto', marginRight: 'auto' }}>
-            <Card.Header style={{ textAlign: 'center' }}>Labrador</Card.Header>
+        <Card style={{ width: '400px', height: '280px', marginLeft: 'auto', marginRight: 'auto' }}>
+            <Card.Header style={{ textAlign: 'center', fontSize: '22px' }}>Labrador</Card.Header>
             <Card.Body>
-                <Card.Title style={{ fontWeight: 'bold', textAlign: 'center' }}>$15/mo</Card.Title>
-                <Card.Text>Unlimited Matches</Card.Text>
-                <Card.Text>Unlimited Messages</Card.Text>
-                <Card.Text>Unlimited App Usage</Card.Text>
+                <Card.Title style={{ fontWeight: 'bold', textAlign: 'center', fontSize: '35px' }}>
+                    $15/mo
+                </Card.Title>
+                <Card.Text style={{ textAlign: 'center', marginBottom: '0' }}>
+                    Unlimited Matches
+                </Card.Text>
+                <Card.Text style={{ textAlign: 'center', marginBottom: '0' }}>
+                    Unlimited Messages
+                </Card.Text>
+                <Card.Text style={{ textAlign: 'center', marginBottom: '0' }}>
+                    Unlimited App Usage
+                </Card.Text>
+                <Button
+                    style={{
+                        backgroundColor: 'black',
+                        marginTop: '15px',
+                        height: '50px',
+                        width: '360px',
+                        color: 'white',
+                    }}
+                >
+                    Get Started
+                </Button>
             </Card.Body>
         </Card>
     );
